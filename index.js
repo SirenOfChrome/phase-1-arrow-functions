cc6097594f8c418d8f3b1b8ecd0cccc76923d5fb
forrestjones <forrestjones2010@gmail.com>
2023-01-10 08:58:05 -0600

    fist successful lab completed praise God

---
+++ index.js
@@ -0,0 +1,18 @@
+const divide = function(a, b) {
+    return a / b;
+
+}
+let a = 2000
+let b = 100
+
+const square = function(x) {
+    return x * x;
+}
+let x = 2
+
+const add = function(p, y) {
+    return p + y;
+}
+let p = 3
+let y = 4
+
